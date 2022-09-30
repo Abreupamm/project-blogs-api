ee234e0f46f284b01a1076000fc3af66cc4b8cb0
Pamela <abrepamm@gmail.com>
2022-09-30 15:34:57 -0300

    reqs 8 e 9

---
+++ src/middlewares/valiedCategoryName.js
@@ -0,0 +1,13 @@
+const mapError = require('../utils/error');
+
+const nameIsValied = (req, res, next) => {
+  const { name } = req.body;
+
+  if (name && name.length > 0) {
+    return res.status(mapError('IS_REQUIRED')).json({ message: '"name" is required' });
+  }
+
+  next();
+};
+
+module.exports = nameIsValied;
