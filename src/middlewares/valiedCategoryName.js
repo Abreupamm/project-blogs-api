07c2c197d179ed6cbf65b4e442e7cb114413d36b
Pamela <abrepamm@gmail.com>
2022-09-30 15:54:36 -0300

    middleware validCatgoryName

--- src/middlewares/valiedCategoryName.js
+++ src/middlewares/valiedCategoryName.js
@@ -3,7 +3,7 @@ const mapError = require('../utils/error');
 const nameIsValied = (req, res, next) => {
   const { name } = req.body;
 
-  if (name && name.length > 0) {
+  if (!name || name.length < 1) {
     return res.status(mapError('IS_REQUIRED')).json({ message: '"name" is required' });
   }
 
--- src/routers/categories.router.js
+++ src/routers/categories.router.js
@@ -3,8 +3,9 @@ const express = require('express');
 const router = express.Router();
 const categoriesController = require('../controller/categories.controller');
 const tokenIsValied = require('../middlewares/valiedToken');
+const nameIsValied = require('../middlewares/valiedCategoryName');
 
-router.post('/', tokenIsValied, categoriesController.postCategory);
+router.post('/', tokenIsValied, nameIsValied, categoriesController.postCategory);
 router.get('/', tokenIsValied, categoriesController.getCategories);
 
 module.exports = router;
